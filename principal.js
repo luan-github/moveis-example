31b45df8400cd7918a21b0305f10e1d75e6fd4a0
Luan Silva da Silva <luansstkd@gmail.com>
2020-01-05 16:32:52 -0300

    inserindo arquivo principal

---
+++ principal.js
@@ -0,0 +1,8 @@
+var banners=["Os melhores do Brasil!", "Qualidade e preço baixo!"];
+var bannerAtual=0;
+
+functiontrocaBanner() {
+	bannerAtual= (bannerAtual+1) %2;document.querySelector(’h2#mensagem’).textContent=banners[bannerAtual];
+}
+
+setInterval(trocaBanner,2000);
